aad399adc62a4a6627f0dccb8d877720e2c0f92b
Lu73-star <992097617luciano@gmail.com>
2025-12-10 13:37:34 -0300

    Refactor build.gradle.kts to simplify plugin management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,10 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
+plugins {
+    // Plugin do aplicativo Android (sem aplicar aqui, só declarando)
+    id("com.android.application") version "8.5.0" apply false
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+    // Plugin Kotlin Android
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 
-rootProject.name = "auryn-offline-01"
-include(":app")
+    // Plugin Hilt (se não usar Hilt, pode remover estas duas linhas)
+    id("com.google.dagger.hilt.android") version "2.52" apply false
+}
