1ceb1e8e71685da2a1b0498b199cc3e00b72bc25
Lu73-star <992097617luciano@gmail.com>
2025-12-10 15:20:40 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,19 @@
-plugins {
-    // Plugin do aplicativo Android
-    id("com.android.application") version "8.5.0" apply false
-
-    // Plugin Kotlin Android
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AurynOffline"
+
+include(":app")
