140736aa0f23aaadfc2525e3d1a7954dc08f1431
Lu73-star <992097617luciano@gmail.com>
2025-12-10 17:38:41 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,47 +1,28 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
+    kotlin("android")
 }
 
 android {
-    namespace = "com.auryn.offline"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.auryn.offline"
-        minSdk = 24
+        applicationId = "com.example.aurynoffline"
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+        versionName = "0.1.0"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-
-    implementation("com.google.dagger:hilt-android:2.52")
-    kapt("com.google.dagger:hilt-android-compiler:2.52")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
