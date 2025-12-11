679e2b315f203fe6d5be977e9b3944306e8fc255
Lu73-star <992097617luciano@gmail.com>
2025-12-11 16:51:42 -0300

    Clean up build.gradle.kts by removing comments
    Removed commented file header and unnecessary whitespace.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-// Arquivo: app/build.gradle.kts
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -47,13 +45,11 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
-    // Testes
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
