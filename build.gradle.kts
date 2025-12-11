b653bca82316b867f2906b8192ac15b1266c93ea
Lu73-star <992097617luciano@gmail.com>
2025-12-10 22:12:34 -0300

    Update comments in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 // Arquivo: build.gradle.kts (RAIZ)
 
 plugins {
-    // Plugin do Android e Kotlin serão aplicados somente nos módulos (como app/)
-    // Aqui usamos o version catalog automático do Gradle moderno
+    // Plugins aplicados somente aos módulos
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
